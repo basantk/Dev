b45b27294a2be1db1ed645bc92137c3b1ceddb13
basantk <basantk21@gmail.com>
2019-05-08 19:55:12 +0530

    Update important_interview_question.php

--- important_interview_question.php
+++ important_interview_question.php
@@ -1,1 +1,24 @@
 <?php 
+  1. Stack and Queue
+  2. array merge, array filter, array walk
+  3. Mysql Conditional query
+  4. memory location of python and php
+    5. REST API
+    6. Token Refreshing
+    7. Redis Architechture, why this is fast, and how they works
+    8. git fetch and git pull
+    9. how many state of AJAX
+    10. what is ob_start()
+    11. late static bindding
+    12. closure function
+    13. anonymus function
+    14. Session and cookie related quetion
+    15. How to make LRU system
+    16. write a program to resize image
+    17. payment gate ways..
+    18. mysql fifth highest salary
+    19. curl syntex and post method....
+    20. oops concept , static member variable and static function how to call
+    21. let and var diff in js
+    22. INput out put.
+    23. OOPS Concept
